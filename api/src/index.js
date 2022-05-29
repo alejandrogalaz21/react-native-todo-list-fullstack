ed1889713757584b89968e3549f2c0c7a42c4759
alejandro galaz <alejandrogalaz21@gmail.com>
2022-05-29 16:50:24 -0500

    add configs

--- api/src/index.js
+++ api/src/index.js
@@ -1,1 +1,12 @@
-console.log('Hello World !')
+import http from 'http'
+import app from './app'
+
+const PORT = 5000
+
+http.Server(app).listen(PORT, async () => {
+  try {
+    console.log(`Api started: http://localhost:${PORT}/api/`)
+  } catch (error) {
+    console.error(error)
+  }
+})
